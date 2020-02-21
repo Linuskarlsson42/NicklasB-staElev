a06330d5e30210a8a5fc441b11f54abb10c32025
Linuskarlsson42 <linus.che@hotmail.com>
2020-02-21 13:54:52 +0100

    delar av uppgift 1 är klar,

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3003
+const port = 3000
 
 var clientDir = __dirname + '\\client\\'
 
 app.get('/', (req, res) => {
-  res.sendFile(clientDir + 'index.html')
+  res.sendFile(clientDir + 'Uppgift1.html')
 })
 app.listen(port, () => console.log(`Example app listening on port port!`))
+
+app.get('/Uppgift1.css', (req,res) => {
+  res.sendFile(clientDir + 'Uppgift1.css' )
+})
+
+app.get('/heh', (req,res)=>{
+  res.sendFile(clientDir + 'ChicksHiggis.jpg')
+})
+
+app.post('/form', (req,res) =>{
+  const name = req.body.name
+  console.log(name)
+})
