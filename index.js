16b340672ba54b14b74231208a359b4d536c2c14
Linuskarlsson42 <linus.che@hotmail.com>
2020-03-02 10:49:55 +0100

    kopierat

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 var clientDir = __dirname + '\\client\\'
 
 app.get('/', (req, res) => {
   res.sendFile(clientDir + 'Uppgift1.html')
 })
-app.listen(port, () => console.log(`Example app listening on port port!`))
 
 app.get('/Uppgift1.css', (req,res) => {
   res.sendFile(clientDir + 'Uppgift1.css' )
@@ -21,3 +23,5 @@ app.post('/form', (req,res) =>{
   const name = req.body.name
   console.log(name)
 })
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
